f207433cb59aa17cc5e1437174da5fa35414c97e
Blair Edwards (IBM) <60191257+bedwards-ibm@users.noreply.github.com>
2025-05-07 10:21:26 +0100

    Update generic_multimodal_data_module.py

--- terratorch/datamodules/generic_multimodal_data_module.py
+++ terratorch/datamodules/generic_multimodal_data_module.py
@@ -512,6 +512,7 @@ class GenericMultiModalDataModule(NonGeoDataModule):
             self.predict_dataset = self.dataset_class(
                 data_root=self.predict_root,
                 num_classes=self.num_classes,
+                image_grep=self.image_grep,
                 allow_substring_file_names=self.allow_substring_file_names,
                 dataset_bands=self.predict_dataset_bands,
                 output_bands=self.predict_output_bands,
